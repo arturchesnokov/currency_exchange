5c2e03e9b22ceb49ba2e42a3f7d7c1da4918a080
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-02-13 20:30:42 +0200

    project init

---
+++ check.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+python src/manage.py check
+flake8 src
+python src/manage.py test
---
+++ src/currency_exchange/settings_local.dev.py
@@ -0,0 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'nqq=gk7dj)0et#lc#eg5c(^hakixx5u&bo4uyl%x7p)gj#%!y)'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
