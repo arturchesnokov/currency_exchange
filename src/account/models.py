de8c767fcef8ce4bf39ef7229456dd1eae251abd
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-03-03 00:15:17 +0200

    rev Revert "hw"
    This reverts commit 86f526819c182a36296ea245fc83eb3f02b4c91b.

--- src/account/models.py
+++ src/account/models.py
@@ -4,11 +4,3 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
-
-
-class Contact(models.Model):
-    email = models.EmailField(unique=True)
-    title = models.CharField(max_length=150)
-    text = models.CharField(max_length=1000)
-    created = models.DateTimeField(auto_now_add=True)
-
--- src/currency_exchange/settings_local.dev.py
+++ src/currency_exchange/settings_local.dev.py
@@ -2,27 +2,9 @@
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '___'
+SECRET_KEY = 'nqq=gk7dj)0et#lc#eg5c(^hakixx5u&bo4uyl%x7p)gj#%!y)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = '___'
-EMAIL_HOST_PASSWORD = '___'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '___',
-        'USER': '___',
-        'PASSWORD': '___',
-        'HOST': '___',
-        'PORT': '5432',
-    },
-}
