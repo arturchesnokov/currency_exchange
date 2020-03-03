69f071262330b6415df0406eeff627d5b9e8537c
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-03-04 00:15:01 +0200

    send

--- src/account/models.py
+++ src/account/models.py
@@ -7,8 +7,7 @@ class User(AbstractUser):
 
 
 class Contact(models.Model):
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     title = models.CharField(max_length=150)
     text = models.CharField(max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
-
--- src/account/urls.py
+++ src/account/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from account.views import SignUp, UserCreate
+from account.views import SignUp, UserCreate, ContactForm
 
 app_name = 'account'
 
 urlpatterns = [
     # path('signup/', SignUp.as_view(), name='signup'),
     path('registration/', UserCreate.as_view(), name='registration'),
+    path('contact/', ContactForm.as_view(), name='contact'),
 ]
