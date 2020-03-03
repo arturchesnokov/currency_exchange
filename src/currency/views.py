1c0ccac26cefa825f112b6437cff84bd69de36b6
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-03-04 00:52:16 +0200

    views and links

--- src/currency/views.py
+++ src/currency/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from currency.models import Rate
+
+
+def rates(request):
+    queryset = Rate.objects.all()
+    return render(request, 'rates.html', context={'rates': queryset})
