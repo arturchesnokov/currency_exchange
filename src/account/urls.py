c6773eb9627fa7a11d9bb4444910939275d2b8f5
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-03-09 00:16:55 +0200

    lesson

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from account.views import SignUp, UserCreate, ContactForm
+from account.views import SignUp, UserCreate, ContactForm, MyProfile
 
 app_name = 'account'
 
@@ -9,4 +9,7 @@ urlpatterns = [
     # path('signup/', SignUp.as_view(), name='signup'),
     path('registration/', UserCreate.as_view(), name='registration'),
     path('contact/', ContactForm.as_view(), name='contact'),
+    path('profile/<int:pk>/', MyProfile.as_view(), name='profile'),
 ]
+
+
--- src/currency_exchange/urls.py
+++ src/currency_exchange/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from django.conf import settings
 
@@ -16,7 +17,9 @@ urlpatterns = [
 
 ]
 
-# if settings.DEBUG:
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 #     import debug_toolbar
 #
 #     urlpatterns = [
