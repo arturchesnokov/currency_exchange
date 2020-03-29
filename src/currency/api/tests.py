e73b722e9b9aeaf5497fd0d36a158c87b3d1aadb
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-03-29 20:13:36 +0300

    t

--- src/currency/api/tests.py
+++ src/currency/api/tests.py
@@ -1,26 +1,49 @@
+from rest_framework.test import APITestCase
+
+from account.models import User
 from currency_exchange import settings
 from django.test import TestCase
 from django.urls import reverse
 
 
-class ApiRateTestCase(TestCase):
+class ApiRateTestCase(APITestCase):
     # @classmethod
     # def setUpClass(cls):
-    #     # headers = {
-    #     #     'Authorization': settings.JWT_TOKEN
-    #     # }
-    #     pass
+    #     user = User.objects.create_user('username', 'Pas$w0rd')
+    #     cls.client.force_authenticate(user)
+
+    def create_rate(self, currency, buy_rate, sale_rate, source):
+        user = User.objects.create_user('username', 'Pas$w0rd')
+        self.client.force_authenticate(user)
+        url = reverse('api-currency:rates')
+        data = {"currency": currency,
+                "buy": buy_rate,
+                "sale": sale_rate,
+                "source": source}
+        self.client.post(url, data)
+
+    def test_create_rate(self):
+        user = User.objects.create_user('username', 'Pas$w0rd')
+        self.client.force_authenticate(user)
+        url = reverse('api-currency:rates')
+        data = {"currency": "1",
+                "buy": 10,
+                "sale": 10,
+                "source": "1"}
+        response = self.client.post(url, data)
+        print('Resp', response)
+        print('Content', response.content)
+        self.assertEqual(response.status_code, 201)
 
     def test_get_rates(self):
-        headers = {
-            'Authorization': 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTg2NjQ4MzkzLCJqdGkiOiI3ODUxYjJmOWJlYzQ0ODlmOTIxYjdiMjYyM2FhMDZiOCIsInVzZXJfaWQiOjF9.mAeDoUf8sWjB6LcQf9tDEbeGpG9DD8vnS85j7E8cwpM'
-            # 'Authorization': settings.JWT_TOKEN
-        }
-        response = self.client.get(reverse('api-currency:rates'), headers=headers)
-        # print(response.content)
+        ApiRateTestCase.create_rate(self, '1', 10.5, 15.9, '1')
+        # user = User.objects.create_user('username', 'Pas$w0rd')
+        # self.client.force_authenticate(user)
+        response = self.client.get(reverse('api-currency:rates'))
+        print(reverse('api-currency:rates'))
+        print('Resp', response)
+        print('Content', response.content)
         self.assertEqual(response.status_code, 200)
 
     def test_always_pass(self):
         assert True
-
-
