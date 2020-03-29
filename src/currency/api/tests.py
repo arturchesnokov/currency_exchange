99ec636cd55106602175a80cf2161da2d6e86b73
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-03-29 23:12:58 +0300

    tests

--- src/currency/api/tests.py
+++ src/currency/api/tests.py
@@ -1,49 +1,59 @@
 from rest_framework.test import APITestCase
 
 from account.models import User
-from currency_exchange import settings
-from django.test import TestCase
+from currency.models import Rate
 from django.urls import reverse
 
 
 class ApiRateTestCase(APITestCase):
-    # @classmethod
-    # def setUpClass(cls):
-    #     user = User.objects.create_user('username', 'Pas$w0rd')
-    #     cls.client.force_authenticate(user)
 
-    def create_rate(self, currency, buy_rate, sale_rate, source):
+    @staticmethod
+    def create_rate(currency, buy_rate, sale_rate, source):
+        r = Rate(currency=currency, buy=buy_rate, sale=sale_rate, source=source)
+        r.save()
+        return r.id
+
+    @staticmethod
+    def get_rate(rate_id):
+        return Rate.objects.get(id=rate_id)
+
+    def test_get_rates(self):  # GET
+        print('test_get_rates started')
         user = User.objects.create_user('username', 'Pas$w0rd')
         self.client.force_authenticate(user)
-        url = reverse('api-currency:rates')
-        data = {"currency": currency,
-                "buy": buy_rate,
-                "sale": sale_rate,
-                "source": source}
-        self.client.post(url, data)
 
-    def test_create_rate(self):
+        ApiRateTestCase.create_rate('1', 10.5, 15.93, '1')
+        ApiRateTestCase.create_rate('1', 11.12, 18.4, '1')
+        ApiRateTestCase.create_rate('2', 12.55, 19.5, '1')
+
+        response = self.client.get(reverse('api-currency:rates'))
+        print('Response->json: ', response.json())
+        assert response.status_code == 200
+        assert len(response.json()) == 3
+
+    def test_create_rate(self):  # POST
+        print("test_create_rate started")
         user = User.objects.create_user('username', 'Pas$w0rd')
         self.client.force_authenticate(user)
         url = reverse('api-currency:rates')
         data = {"currency": "1",
-                "buy": 10,
-                "sale": 10,
+                "buy": 10.55,
+                "sale": 20,
                 "source": "1"}
         response = self.client.post(url, data)
-        print('Resp', response)
-        print('Content', response.content)
-        self.assertEqual(response.status_code, 201)
+        print('Response->json: ', response.json())
+        assert response.status_code == 201
+        r_id = response.json()['id']
+        assert ApiRateTestCase.get_rate(r_id)
 
-    def test_get_rates(self):
-        ApiRateTestCase.create_rate(self, '1', 10.5, 15.9, '1')
-        # user = User.objects.create_user('username', 'Pas$w0rd')
-        # self.client.force_authenticate(user)
-        response = self.client.get(reverse('api-currency:rates'))
-        print(reverse('api-currency:rates'))
-        print('Resp', response)
-        print('Content', response.content)
-        self.assertEqual(response.status_code, 200)
+    def test_get_rate(self):  # GET by id
+        print('test_get_rate started')
+        user = User.objects.create_user('username', 'Pas$w0rd')
+        self.client.force_authenticate(user)
 
-    def test_always_pass(self):
-        assert True
+        r_id = ApiRateTestCase.create_rate('1', 11.11, 22.22, '1')
+
+        response = self.client.get(reverse('api-currency:rate', args=(r_id,)))
+        print('Response->json: ', response.json())
+        assert response.status_code == 200
+        assert response.json()['id'] == r_id
